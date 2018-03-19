ba387616dec00dca9c4ee996b74184f814b0a773
PeterGao <757789447@qq.com>
2018-03-19 21:19:33 +0800

    项目初始结构

---
+++ src/component/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <header></header>
+        <app-log></app-log>
+        <footer></footer>
+    </div>
+</template>
+
+<script>
+    export default{
+
+    }
+</script>
+
+<style scoped>
+
+</style>
